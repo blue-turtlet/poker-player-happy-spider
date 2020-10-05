2b085ad83f7d16fa66abcbc4ff9e5435629a8121
Joseph Ashwin Kottapurath <ashwinkjoseph@gmail.com>
2020-10-05 23:37:32 +0530

    feat: initial strategy

--- Player.js
+++ Player.js
@@ -7,7 +7,16 @@ class Player {
 
   static betRequest(gameState, bet) {
     var game = new GameState(gameState);
-    bet(0);
+    const me = game.me();
+    let score = 0;
+    if (me.hasPocketPair()) {
+      score += 10;
+    }
+    const highestRank = me.highestPocketValue();
+    if (highestRank > 10) {
+      score += Math.round(highestRank/2);
+    }
+    bet(score);
   }
 
   static showdown(gameState) {
